eb92d8811a3cbe1a179328837b32ee705b18dfb4
TacoBurritoGuaco <65143885+TacoBurritoGuaco@users.noreply.github.com>
2024-03-14 14:51:53 -0400

    Task 2 Complete Used Enum to give each villager type its own ChestType Chests with specific chest types can only be opened by specific villagers with those chestTypes This is done using overrides

--- Assets/Week 9/Scripts/Chest.cs
+++ Assets/Week 9/Scripts/Chest.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ChestType { Villager, Mechant, Archer }
 public class Chest : MonoBehaviour
 {
     public Animator animator;
-    
+    public ChestType whoCanOpen; //enum
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        animator.SetBool("IsOpened", true);
+        if(collision.TryGetComponent<Villager>(out Villager villager)) //REMEMBER THIS LATER
+        {
+            if (villager.CanOpen() == whoCanOpen || whoCanOpen == ChestType.Villager)
+            {
+                animator.SetBool("IsOpened", true);
+            }
+        }  
     }
 
     private void OnTriggerExit2D(Collider2D collision)
