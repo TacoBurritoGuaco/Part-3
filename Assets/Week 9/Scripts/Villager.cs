d8bbb91b1f59a2709ba15c09cb6df48c6f156c80
TacoBurritoGuaco <65143885+TacoBurritoGuaco@users.noreply.github.com>
2024-03-21 15:13:15 -0400

    Completed Task2 Created UI Elements and Scene Created Slider UI, Timer, and Options This will be VERY useful for the project

--- Assets/Week 9/Scripts/Villager.cs
+++ Assets/Week 9/Scripts/Villager.cs
@@ -64,7 +64,7 @@ public class Villager : MonoBehaviour
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
     }
 
-    protected virtual void Update()
+    private void Update()
     {
         //left click: move to the click location
         if (Input.GetMouseButtonDown(0) && isSelected && !clickingOnSelf)
