9f41235331af45b27c5afa6a3b12fce037dfdf17
TacoBurritoGuaco <65143885+TacoBurritoGuaco@users.noreply.github.com>
2024-03-21 17:07:18 -0400

    Task-4a completed Villagers now have their names displayed above them Villagers are now selected using a dropdown UI element

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -7,7 +7,9 @@ using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
     public TextMeshProUGUI selectUI;
+    public List<Villager> villagerList = new List<Villager>();
     public static CharacterControl intance; //A variable that is a reference to a particular object of this class
     //Because it is a static variable, it allows us to speak with it in a static function
     public static Villager SelectedVillager { get; private set; }
@@ -29,6 +31,10 @@ public class CharacterControl : MonoBehaviour
         intance.selectUI.text = SelectedVillager.name;
     }
 
+    public void changeCharacter(int value)
+    {
+        SetSelectedVillager(villagerList[value]);
+    }
     ////void Update()
     ////{
     //    if (!SelectedVillager.IsUnityNull())
